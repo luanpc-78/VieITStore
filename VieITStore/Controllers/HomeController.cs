3c54beed829134981557f033b5f3b221b308df56
Phan Cong Luan <luanpc78@gmail.com>
2026-08-06 21:59:39 +0700

    feat(storefront): add home page and shared navigation

--- VieITStore/Controllers/HomeController.cs
+++ VieITStore/Controllers/HomeController.cs
@@ -1,17 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using VieITStore.Models;
 
 namespace VieITStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Index()
+        {
+            var sanPhamNoiBat = await _context.SanPhams
+                .Include(s => s.DanhMuc)
+                .Where(s => s.NoiBat && s.TrangThai)
+                .Take(8)
+                .ToListAsync();
+
+            var sanPhamMoi = await _context.SanPhams
+                .Include(s => s.DanhMuc)
+                .Where(s => s.TrangThai)
+                .OrderByDescending(s => s.NgayTao)
+                .Take(8)
+                .ToListAsync();
+
+            ViewBag.DanhMucs = await _context.DanhMucs
+                .Where(d => d.HienThi)
+                .ToListAsync();
+
+            ViewBag.SanPhamMoi = sanPhamMoi;
+            return View(sanPhamNoiBat);
+        }
+
+        public IActionResult AccessDenied()
         {
             return View();
         }
@@ -19,7 +44,8 @@ namespace VieITStore.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            Response.StatusCode = StatusCodes.Status500InternalServerError;
+            return View();
         }
     }
 }
--- VieITStore/wwwroot/js/site.js
+++ VieITStore/wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
 
-// Write your JavaScript code.
+// Format tiền VNĐ
+function formatVND(amount) {
+    return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(amount);
+}
